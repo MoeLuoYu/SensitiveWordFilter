74e06315736bac2e2e360c995a6e4b35767819b7
落雨 <69842604+MoeLuoYu@users.noreply.github.com>
2025-02-06 16:40:42 +0800

    增加书本检测

--- src/main/java/xyz/moeluoyu/sensitivewordfilter/listener/ListenerClass.java
+++ src/main/java/xyz/moeluoyu/sensitivewordfilter/listener/ListenerClass.java
@@ -5,7 +5,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerEditBookEvent;
 import org.bukkit.inventory.AnvilInventory;
