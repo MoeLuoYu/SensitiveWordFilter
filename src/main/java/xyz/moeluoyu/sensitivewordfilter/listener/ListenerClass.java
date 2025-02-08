b2d53297cde00ef391ca1fb4ef29b6d67ace81c8
落雨 <69842604+MoeLuoYu@users.noreply.github.com>
2025-02-09 00:12:16 +0800

    增加豁免权限检测

--- src/main/java/xyz/moeluoyu/sensitivewordfilter/listener/ListenerClass.java
+++ src/main/java/xyz/moeluoyu/sensitivewordfilter/listener/ListenerClass.java
@@ -1,6 +1,7 @@
 package xyz.moeluoyu.sensitivewordfilter.listener;
 
 import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.SignChangeEvent;
@@ -25,10 +26,12 @@ public class ListenerClass implements Listener {
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
+        // 获取玩家对象
+        Player player = event.getPlayer();
         // 获取玩家发送的原始消息
         String message = event.getMessage();
         // 使用敏感词过滤器过滤消息
-        String filteredMessage = filter.filter(message);
+        String filteredMessage = filter.filter(player, message);
         // 将过滤后的消息设置回事件中，以便发送给其他玩家
         event.setMessage(filteredMessage);
     }
@@ -38,6 +41,8 @@ public class ListenerClass implements Listener {
     public void onAnvilClick(InventoryClickEvent event) {
         if (event.getInventory() instanceof AnvilInventory) {
             AnvilInventory anvil = (AnvilInventory) event.getInventory();
+            // 获取玩家对象
+            Player player = (Player) event.getWhoClicked();
             // 获取铁砧中第一个物品（待命名的物品）
             ItemStack item = anvil.getItem(0);
             if (item != null) {
@@ -47,7 +52,7 @@ public class ListenerClass implements Listener {
                     ItemMeta meta = result.getItemMeta();
                     if (meta.hasDisplayName()) {
                         String name = meta.getDisplayName();
-                        String filteredName = filter.filter(name);
+                        String filteredName = filter.filter(player, name);
                         // 如果名称被修改，更新显示名称
                         if (!name.equals(filteredName)) {
                             meta.setDisplayName(filteredName);
@@ -63,6 +68,9 @@ public class ListenerClass implements Listener {
 
     @EventHandler
     public void onSignChange(SignChangeEvent event) {
+        // 获取玩家对象
+        Player player = event.getPlayer();
+
         // 获取告示牌的状态
         Sign sign = (Sign) event.getBlock().getState();
         // 遍历告示牌的每一行
@@ -70,7 +78,7 @@ public class ListenerClass implements Listener {
             // 获取当前行的文字
             String line = event.getLine(i);
             // 过滤当前行的文字
-            String filteredLine = filter.filter(line);
+            String filteredLine = filter.filter(player, line);
             // 设置过滤后的文字到当前行
             event.setLine(i, filteredLine);
         }
@@ -81,25 +89,27 @@ public class ListenerClass implements Listener {
         BookMeta bookMeta = event.getNewBookMeta();
         List<String> pages = bookMeta.getPages(); // 获取书本的页面内容
 
+        // 获取玩家对象
+        Player player = event.getPlayer();
         // 创建一个新的可变 List 来存储过滤后的页面内容
         List<String> filteredPages = new ArrayList<>();
         // 过滤书本的标题（名称）
         String originalTitle = bookMeta.getTitle();
         if (originalTitle != null) {
-            String filteredTitle = filter.filter(originalTitle);
+            String filteredTitle = filter.filter(player,originalTitle);
             bookMeta.setTitle(filteredTitle);
         }
 
         // 过滤书本的作者
         String originalAuthor = bookMeta.getAuthor();
         if (originalAuthor != null) {
-            String filteredAuthor = filter.filter(originalAuthor);
+            String filteredAuthor = filter.filter(player,originalAuthor);
             bookMeta.setAuthor(filteredAuthor);
         }
 
         // 遍历书本的每一页，进行敏感词过滤
         for (String pageContent : pages) {
-            String filteredContent = filter.filter(pageContent);
+            String filteredContent = filter.filter(player,pageContent);
             filteredPages.add(filteredContent);
         }
 
